4691080f0e664353144cd55d0186bb339ba99262
AlexeyB163 <sourceaisa@mail.ru>
2021-12-22 19:38:52 +0400

    work with logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+
+    @IBOutlet weak var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .green
+        showTextButton.layer.cornerRadius = 10
+    } 
+// Тип элемента в sender в параметрах нужен если мы хотим изменять атрибуты элемента. Но у нас уже есть этот аутлет, поэтому здесь мы не работаем с sender. Sender нужно убрать.
+
+    @IBAction func showTextButtonPressed() {
+        helloWorldLabel.isHidden.toggle() // toogle меняет значение на противоположное
+       
+       if helloWorldLabel.isHidden {
+           showTextButton.setTitle("Show text", for: .normal)
+       } else {
+           showTextButton.setTitle("Hide text", for: .normal)
+       }
     }
-
-
+    
 }
 
